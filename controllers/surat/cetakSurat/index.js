bdab2eb34c689291bf0687659f73f13143957879
alishaileen <alishaileen05@gmail.com>
2021-06-16 21:24:58 +0700

    added cetak surat izin ekaristi

--- controllers/surat/cetakSurat/index.js
+++ controllers/surat/cetakSurat/index.js
@@ -35,6 +35,14 @@ const cetakSurat = async (req, res) => {
     
     const data = await getData(jenisSurat, id)
 
+    console.log(data)
+
+    if (data === 404) {
+      return res.status(404).send({
+        message: 'Data not found'
+      });
+    }
+
     const content = await compile(getTemplateSurat(jenisSurat), data)
 
     await page.setContent(content)
@@ -93,12 +101,58 @@ const getDataSuratKeterangan = async (id) => {
         JOIN Lingkungan L ON (S.id_lingkungan=L.id)
         JOIN Admin Sekret ON (S.id_sekretariat=Sekret.id)
         JOIN (SELECT id, nama, role FROM Admin) Romo ON (S.id_romo=Romo.id)
-    WHERE S.id = 11`
+    WHERE S.id = ?`
     let result = await db(sql, [ id ])
     
     if(result.length === 0) {
       return 404
     } else {
+      return result[0]
+    }
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+const getDataSuratIzinEkaristi = async (id) => {
+  try {
+    let sql = 
+    `SELECT S.id,
+            S.no_surat,
+            L.nama_lingkungan,
+            K.nama_keluarga,
+            K.nama_kepala_keluarga,
+            K.no_telp_kepala_keluarga,
+            S.id_lingkungan,
+            L.nama_lingkungan,
+            DATE_FORMAT(S.tgl_pelaksanaan, '%d-%m-%Y') AS tgl_pelaksanaan,
+            S.waktu_mulai,
+            S.waktu_selesai,
+            S.intensi,
+            S.lokasi_rumah,
+            S.no_telp_lokasi,
+            S.romo_pemimpin,
+            S.alamat_komunitas,
+            S.no_telp_komunitas,
+            S.ketua_lingkungan,
+            S.ketua_lingkungan_approval,
+            Sekret.nama AS nama_sekretariat,
+            S.sekretariat_approval,
+            Romo.nama AS nama_romo_paroki,
+            S.romo_approval,
+            DATE_FORMAT(S.created_at, '%d-%m-%Y') AS created_at
+    FROM Surat_Izin_Pelayanan_Ekaristi S JOIN Keluarga K ON (S.id_keluarga=K.id)
+        JOIN Lingkungan L ON (S.id_lingkungan=L.id)
+        JOIN Admin Sekret ON (S.id_sekretariat=Sekret.id)
+        JOIN (SELECT id, nama, role FROM Admin) Romo ON (S.id_romo=Romo.id)
+    WHERE S.id = ?`
+    let result = await db(sql, [ id ])
+    
+    if(result.length === 0) {
+      return 404
+    } else {
+      result[0].waktu_mulai = result[0].waktu_mulai.substring(0,5)
+      result[0].waktu_selesai = result[0].waktu_selesai.substring(0,5)
       return result[0]
     }
   } catch (error) {
