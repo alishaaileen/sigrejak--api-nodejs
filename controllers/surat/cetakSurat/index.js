11f3385557b1eb875b1e6a49c2f6624a061b7dbb
alishaileen <alishaileen05@gmail.com>
2021-06-18 15:35:59 +0700

    added cetak surat pelayanan minyak suci

--- controllers/surat/cetakSurat/index.js
+++ controllers/surat/cetakSurat/index.js
@@ -15,15 +15,17 @@ const compile = async (templateName, data) => {
 const getData = async (jenisSurat, id) => {
   if (jenisSurat === 'surat-keterangan') {
     return getDataSuratKeterangan(id)
-  }
-  else if (jenisSurat === 'surat-izin-pelayanan-ekaristi') {
+  } else if (jenisSurat === 'surat-izin-pelayanan-ekaristi') {
     return getDataSuratIzinEkaristi(id)
+  } else if (jenisSurat === 'surat-pelayanan-minyak-suci') {
+    return getDataSuratPelayananMinyakSuci(id)
   }
 }
 
 const getTemplateSurat = (jenisSurat) => {
   if (jenisSurat === 'surat-keterangan') return 'suratKeterangan'
   else if (jenisSurat === 'surat-izin-pelayanan-ekaristi') return 'suratIzinEkaristi'
+  else if (jenisSurat === 'surat-pelayanan-minyak-suci') return 'suratMinyakSuci'
 } 
 
 const cetakSurat = async (req, res) => {
@@ -153,6 +155,61 @@ const getDataSuratIzinEkaristi = async (id) => {
     } else {
       result[0].waktu_mulai = result[0].waktu_mulai.substring(0,5)
       result[0].waktu_selesai = result[0].waktu_selesai.substring(0,5)
+      return result[0]
+    }
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+const getDataSuratPelayananMinyakSuci = async (id) => {
+  try {
+    let sql = 
+    `SELECT S.id,
+            S.no_surat,
+            L.nama_lingkungan,
+            S.id_keluarga,
+            S.id_lingkungan,
+            S.nama_keluarga_penanggung_jawab,
+            S.alamat_keluarga_penanggung_jawab,
+            S.no_telp_keluarga_penanggung_jawab,
+            S.nama,
+            S.nama_baptis,
+            S.tempat_lahir,
+            DATE_FORMAT(S.tgl_lahir, '%d-%m-%Y') AS tgl_lahir,
+            S.alamat,
+            S.nama_pasangan,
+            S.cara_menikah,
+            S.tahun_menikah,
+            S.status_terima_minyak,
+            DATE_FORMAT(S.tgl_terima_minyak, '%d-%m-%Y') AS tgl_terima_minyak,
+            S.id_pastor_pelayan,
+            R.nama_pastor_pelayan,
+            S.pastor_pelayan_approval,
+            S.ketua_lingkungan,
+            S.ketua_lingkungan_approval,
+            S.id_sekretariat,
+            S.sekretariat_approval,
+            DATE_FORMAT(S.created_at, '%d-%m-%Y') AS created_at,
+            DATE_FORMAT(S.updated_at, '%d-%m-%Y') AS updated_at,
+            DATE_FORMAT(S.deleted_at, '%d-%m-%Y') AS deleted_at
+        FROM Surat_Pelayanan_Minyak_Suci S JOIN (SELECT id AS id_pastor_pelayan, nama AS nama_pastor_pelayan FROM Admin) R ON (S.id_pastor_pelayan=R.id_pastor_pelayan)
+        JOIN Lingkungan L ON (S.id_lingkungan=L.id)
+        JOIN Admin Sekret ON (S.id_sekretariat=Sekret.id)
+    WHERE S.id = ?`
+    let result = await db(sql, [ id ])
+    
+    if(result.length === 0) {
+      return 404
+    } else {
+      if(result[0].nama_pasangan === null) {
+        result[0].nama_pasangan = '-'
+        result[0].cara_menikah = '-'
+        result[0].tahun_menikah = '-'
+      }
+      if(result[0].status_terima_minyak === 'Belum pernah') {
+        result[0].tgl_terima_minyak = '-'
+      }
       return result[0]
     }
   } catch (error) {
